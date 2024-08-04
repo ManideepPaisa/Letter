1108c0034e6f7137a1aa34a954bccf0c658c2536
ManideepPaisa <manupaisa17@gmail.com>
2024-08-04 15:06:01 +0530

    Create App.js

---
+++ Src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Envelope from './Envelope';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Love Letter</h1>
+      <Envelope />
+    </div>
+  );
+}
+
+export default App;
